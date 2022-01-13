d5dadeee722e5976bc5bff8396972f9b3e9438f8
Sviatoslav <sviatoslavnazin@gmail.com>
2022-01-13 23:53:59 +0800

    Big boy is defeated

--- Graph/Source.cpp
+++ Graph/Source.cpp
@@ -9,9 +9,82 @@
 
 using namespace std;
 
+typedef vector<Node*>::const_iterator node_iterator;
+
+bool inVector(vector<Node*> nodes, Node* node)
+{
+	for (Node* n : nodes)
+	{
+		if (node == n)
+			return true;
+	}
+	return false;
+}
+
+void setGraph(vector<Node*>& visited, Graph& graph, Node* current, Node* prev = nullptr)
+{	
+	if (prev != nullptr)
+	{
+		graph.addEdge(prev, current);
+	}
+
+	visited.push_back(current);
+
+	for (Node* neighbour : current->getNeighbours())
+	{
+		if (inVector(visited, neighbour))
+			continue;
+
+		prev = current;
+		setGraph(visited, graph, neighbour, prev);
+	}
+}
+
+vector<Graph*> getGraphs(Graph* mainGraph)
+{
+	vector<Graph*> graphs;
+	vector<Node*> visited;
+
+	while (visited.size() != mainGraph->getNodes().size())
+	{
+		Graph* graph = new Graph;
+		Node* begin = nullptr;
+
+		if (visited.size() == 0)
+			begin = *mainGraph->nd_begin();
+		else
+			begin = mainGraph->getNodes()[visited.size() - 1];
+
+		setGraph(visited, *graph, begin);
+
+		graphs.push_back(graph);
+	}
+
+	return graphs;
+}
+
+void writeGraphs(vector<Graph*> graphs)
+{
+	for (size_t i = 0; i < graphs.size(); i++)
+	{
+		string path = "Graph" + to_string(i + 1) + ".dat";
+		ofstream fout(path);
+
+		if (fout.is_open())
+		{
+			fout << *graphs[i];
+			cout << "Graph " << i + 1 << " was successful written" << endl;
+		}
+		else
+		{
+			cout << "Open file error" << endl;
+		}
+	}
+}
+
 int main()
 {
-	string path = "TestGraphBig.txt";
+	string path = "TestGraphSmall.txt";
 
 	ifstream file;
 
@@ -22,7 +95,7 @@ int main()
 		string buf;
 		getline(file, buf);
 
-		Graph mainGraph;
+		Graph* mainGraph = new Graph;
 
 		while (!file.fail())
 		{
@@ -33,69 +106,18 @@ int main()
 			if (source == "" && target == "")
 				continue;
 
-			mainGraph.addEdge(source, target);
+			mainGraph->addEdge(source, target);
 		}
 
 		file.close();
 
-		vector<Graph*> graphs;
-		BFS bfs(mainGraph);
-		
-		for (size_t i = 0; i < mainGraph.getNodes().size() - 1; i++)
-		{
-			Node* begin = mainGraph.getNodes()[i];
-
-			for (size_t j = i + 1; j < mainGraph.getNodes().size(); j++)
-			{
-				Node* end = mainGraph.getNodes()[j];
-
-				if (bfs.connected(begin, end))
-				{
-					bool isFind = false;
-
-					for (Graph* graph : graphs)
-					{
-						if(graph->hasNode(begin) || graph->hasNode(end))
-						{
-							graph->addEdge(begin, end);
-
-							isFind = true;
-
-							break;
-						}
-					}
-
-					if (!isFind)
-					{
-						Graph* graph = new Graph;
-
-						graph->addEdge(begin, end);
-						graphs.push_back(graph);
-					}
-				}
-			}
-		}
-
-		for (size_t i = 0; i < graphs.size(); i++)
-		{
-			string path = "Graph" + to_string(i + 1) + ".dat";
-			ofstream fout(path);
-
-			if (fout.is_open())
-			{
-				fout << *graphs[i];
-				cout << "Graph " << i + 1 << " was successful written" << endl;
-			}
-			else
-			{
-				cout << "Open file error" << endl;
-			}
-		}
+		vector<Graph*> graphs = getGraphs(mainGraph);
+		writeGraphs(graphs);
 	}
 	else
 	{
 		cout << "Open file error" << endl;
 	}
-
+		
 	return 0;
 }
